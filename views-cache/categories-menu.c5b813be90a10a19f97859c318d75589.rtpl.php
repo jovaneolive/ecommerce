3b3e58716e6ba711964f9636dbdd97dba1785624
Jovane de Oliveira <jovaneolive@hotmail.com>
2021-01-16 17:21:03 -0300

    Detalhes do Produto

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/3">Google</a></li><li><a href="/categories/7">Samsung</a></li><li><a href="/categories/5">Xaomi</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/3">Google</a></li><li><a href="/categories/8">Motorola</a></li><li><a href="/categories/7">Samsung</a></li><li><a href="/categories/5">Xaomi</a></li>
