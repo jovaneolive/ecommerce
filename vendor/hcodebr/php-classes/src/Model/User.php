ab61ae8913e50b2fa77425ce42d040e2bfa2ad1b
Jovane de Oliveira <jovaneolive@hotmail.com>
2021-01-23 11:11:52 -0300

    Minha conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model {
 	const SECRET_IV = "HcodePhp7_Secret_IV";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSuccess";
 
 	public static function getFromSession() {
 
@@ -365,6 +366,28 @@ class User extends Model {
 	public static function clearError () {
 
 		$_SESSION[User::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess ($msg) {
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess () {
+
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess () {
+
+		$_SESSION[User::SUCCESS] = NULL;
 
 	}
 
